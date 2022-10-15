d13521c94715e58d70f1b766cb047f9ce6def82a
Nikolaj Sommer <nikolaj.jensen@gmail.com>
2022-10-15 23:31:11 +0200

    v1.0

--- update-install/GenerateVersionsFile.php
+++ update-install/GenerateVersionsFile.php
@@ -6,58 +6,19 @@
 $VERSIONS_FILE = "versions.json";
 
 $output = [
-    'stable' => '0.9.4',
+    'stable' => '1.0',
     'versions' => [
-        '0.9.0' => [
-            'branch' => 'v0.9.0', // The branch name in the git repository
+        '1.0' => [
+            'branch' => 'v1.0', // The branch name in the git repository
             'summary' => 'Initial release', // A short description of the release
-            'date' => '2022-10-12', // The date of the release
+            'date' => '2022-10-15', // The date of the release
             'changelog' => [ // A list of changes in this release
                 'Added feature: Vhost management',
                 'Added feature: Multiple php version management',
                 'Added feature: SSH / Deploy keys management',
                 // .... more changelog entries
             ]
-        ],
-        '0.9.1' => [
-            'branch' => 'v0.9.1', // The branch name in the git repository
-            'summary' => 'Pre release 1', // A short description of the release
-            'date' => '2022-10-12', // The date of the release
-            'changelog' => [ // A list of changes in this release
-                'Removed all web-api related code',
-                // .... more changelog entries
-            ]
-        ],
-        '0.9.2' => [
-            'branch' => 'v0.9.2', // The branch name in the git repository
-            'summary' => 'Pre release 2', // A short description of the release
-            'date' => '2022-10-12', // The date of the release
-            'changelog' => [ // A list of changes in this release
-                'Only updated docs',
-                'This release showcases the update mechanism',
-                // .... more changelog entries
-            ]
-        ],
-        '0.9.3' => [
-            'branch' => 'v0.9.3', // The branch name in the git repository
-            'summary' => 'Pre release 3', // A short description of the release
-            'date' => '2022-10-14', // The date of the release
-            'changelog' => [ // A list of changes in this release
-                'Docs updated',
-                'Update info fetched from github',
-                // .... more changelog entries
-            ]
-        ],
-        '0.9.4' => [
-            'branch' => 'master', // The branch name in the git repository
-            'summary' => 'Pre release 4', // A short description of the release
-            'date' => '2022-10-15', // The date of the release
-            'changelog' => [ // A list of changes in this release
-                'Certbot installation + configuration',
-                // .... more changelog entries
-            ]
-        ],
-        // .... add more versions here
+            ],
     ]
 ];
 
